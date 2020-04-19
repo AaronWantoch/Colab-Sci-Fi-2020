debc0bbc7280557a61ce63bde3792b766834b2a7
credus <aaron.wantoch@gmail.com>
2020-04-19 23:20:00 +0200

    Added animations

---
+++ Assets/Scripts/AI/GuardAnimationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class GuardAnimationController : MonoBehaviour
+{
+    NavMeshAgent agent;
+    ThirdPersonCharacter character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        character = GetComponent<ThirdPersonCharacter>();
+
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
