d60643cf2c65cdd36fda75fc3ffe1d81cb4c86b7
credus <aaron.wantoch@gmail.com>
2020-04-29 23:16:04 +0200

    Basic animations are back

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -4,30 +4,31 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityStandardAssets.Characters.ThirdPerson;
 
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
 public class AnimationController : MonoBehaviour
 {
     NavMeshAgent _agent;
     ThirdPersonCharacter _character;
+    Animator _animator;
+
+    int _isWalkingHash = Animator.StringToHash("isWalking");
 
     // Start is called before the first frame update
     void Start()
     {
+        _animator = GetComponent<Animator>();
         _agent = GetComponent<NavMeshAgent>();
-        _character = GetComponent<ThirdPersonCharacter>();
 
-        _agent.updateRotation = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_agent.remainingDistance > _agent.stoppingDistance)
-        {
-            _character.Move(_agent.desiredVelocity, false, false);
-        }
+        if (_agent.speed > 0)
+            _animator.SetBool(_isWalkingHash, true);
         else
-        {
-            _character.Move(Vector3.zero, false, false);
-        }
+            _animator.SetBool(_isWalkingHash, false);
+
     }
 }
