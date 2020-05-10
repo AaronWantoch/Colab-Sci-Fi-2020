a6f0918e8620dacb2b0d1d342339ef1b5ea50b6b
credus <aaron.wantoch@gmail.com>
2020-05-10 23:35:45 +0200

    Started implemention of state machine

---
+++ Assets/Scripts/AI/Guard States/GuardLookingForTargetState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardLookingForTargetState : GuardBaseState
+{
+    public override void EnterState(GuardAI guard)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnCollisionEnter(GuardAI guard)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update(GuardAI guard)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/AI/GuardBaseState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GuardBaseState
+{
+    public abstract void EnterState(GuardAI guard);
+    public abstract void Update(GuardAI guard);
+    public abstract void OnCollisionEnter(GuardAI guard);
+}
